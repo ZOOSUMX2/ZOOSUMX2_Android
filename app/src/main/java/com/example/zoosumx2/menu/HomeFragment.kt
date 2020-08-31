37119175d97127c8b651541932d5bfcd7f60df42
aerim <aerimforest98@gmail.com>
2020-09-01 03:04:22 +0900

    Add speech bubble fade in/out animation Change main page design

--- app/src/main/java/com/example/zoosumx2/menu/HomeFragment.kt
+++ app/src/main/java/com/example/zoosumx2/menu/HomeFragment.kt
@@ -1,14 +1,13 @@
 package com.example.zoosumx2.menu
 
-import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
-import com.example.zoosumx2.MakequizActivity
 import com.example.zoosumx2.R
-import com.example.zoosumx2.ResidentQuizActivity
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -16,23 +15,32 @@ class HomeFragment : Fragment() {
     //Todo: 전역변수 정의
     //Fixme:
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = imagebutton_general_quiz_home
-        button.setOnClickListener {
-            val intent = Intent(context, MakequizActivity::class.java)
-            startActivity(intent)
-        }
+        speech_bubble1.visibility = View.INVISIBLE
+        speech_bubble2.visibility = View.INVISIBLE
 
-        val button2 = imagebutton_resident_quiz_home
-        button2.setOnClickListener {
-            val intent = Intent(context, ResidentQuizActivity::class.java)
-            startActivity(intent)
-        }
+        //loading our custom made animations
+        val fadeIn = AnimationUtils.loadAnimation(context, R.anim.fade_in)
+
+        //fading our custom made animations
+        val fadeOut = AnimationUtils.loadAnimation(context, R.anim.fade_out)
+
+        speech_bubble1.startAnimation(fadeIn)
+
+        Handler().postDelayed({
+            //Do something after 5000ms
+            speech_bubble1.startAnimation(fadeOut)
+            speech_bubble2.startAnimation(fadeIn)
+        }, 5000)
     }
 }
