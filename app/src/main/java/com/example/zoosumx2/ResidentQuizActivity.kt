848ef9b626fea7b39e0fdbecdda6b2452507c6b0
aerim <aerimforest98@gmail.com>
2020-09-13 02:04:04 +0900

    Add scrollView gradient & change backgroundcolor when button clicked

--- app/src/main/java/com/example/zoosumx2/ResidentQuizActivity.kt
+++ app/src/main/java/com/example/zoosumx2/ResidentQuizActivity.kt
@@ -1,31 +1,58 @@
 package com.example.zoosumx2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.core.content.ContextCompat
 
 class ResidentQuizActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resident_quiz)
 
-        val button1 = findViewById<Button>(R.id.correct_answer)
-        button1.setOnClickListener {
-//            fun onClick(view: View) {
-//                button1.setBackgroundColor(R.drawable.item_color)
-//            }
+        val correctAns = findViewById<Button>(R.id.button_correct_answer_quiz)
+        val wrongAns = findViewById<Button>(R.id.button_wrong_answer_quiz)
+
+        correctAns.setOnClickListener {
+            if (wrongAns.isSelected) {
+                wrongAns.isSelected = false
+                wrongAns.setTextColor(Color.BLACK)
+            }
+            correctAns.isSelected = true
+            correctAns.setTextColor(ContextCompat.getColor(this, R.color.friendly_green))
         }
 
-        val button2 = findViewById<ImageButton>(R.id.imagebutton_back_resident_quiz)
-        button2.setOnClickListener{
+        wrongAns.setOnClickListener {
+            if (correctAns.isSelected) {
+                correctAns.isSelected = false
+                correctAns.setTextColor(ContextCompat.getColor(this, R.color.friendly_green))
+            }
+            wrongAns.isSelected = true
+            wrongAns.setTextColor((Color.WHITE))
+        }
+
+        val backButton = findViewById<ImageButton>(R.id.imagebutton_back_resident_quiz)
+        backButton.setOnClickListener {
 //            val intent = Intent(this, MainActivity::class.java)
 //            startActivity(intent)
             finish()
         }
 
-        //Todo : 정답확인 버튼 클릭 이벤트
+        val submitAns = findViewById<Button>(R.id.button_check_answer_resident_quiz)
+        submitAns.setOnClickListener {
+
+            // 정답을 선택한 경우
+            if(correctAns.isSelected) {
+
+            }
+            // 오답을 선택한 경우
+            else {
+
+            }
+        }
 
     }
 }
