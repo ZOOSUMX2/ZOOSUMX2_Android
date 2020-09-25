18ae3b236a129de06f767979ee8db1206fd722d3
aerim <aerimforest98@gmail.com>
2020-09-26 02:34:12 +0900

    connect login activity & main activity

--- app/src/main/java/com/example/zoosumx2/LoginActivity.kt
+++ app/src/main/java/com/example/zoosumx2/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.zoosumx2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val kakaoButton = findViewById<Button>(R.id.button_kakao_login)
+        kakaoButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
