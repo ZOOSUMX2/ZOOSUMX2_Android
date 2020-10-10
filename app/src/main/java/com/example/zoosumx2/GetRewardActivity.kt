d8644320027552618480af6e70fde5e32dfc1029
aerim <aerimforest98@gmail.com>
2020-10-11 01:51:42 +0900

    리워드 애니메이션 추가

--- app/src/main/java/com/example/zoosumx2/GetRewardActivity.kt
+++ app/src/main/java/com/example/zoosumx2/GetRewardActivity.kt
@@ -2,7 +2,9 @@ package com.example.zoosumx2
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class GetRewardActivity : AppCompatActivity() {
@@ -11,8 +13,12 @@ class GetRewardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_reward)
 
+        val finalPoint = findViewById<TextView>(R.id.textview_finalpoint_getreward)
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        finalPoint.startAnimation(fadeIn)
+
         val nextButton = findViewById<Button>(R.id.get_reward_next)
-        nextButton.setOnClickListener{
+        nextButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
