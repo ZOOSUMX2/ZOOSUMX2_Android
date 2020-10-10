7bee7f8a092432bfd131364a4df6d796419b1ae5
aerim <aerimforest98@gmail.com>
2020-10-10 23:52:06 +0900

    Edittext가 비어있는 경우 button 비활성화

--- app/src/main/java/com/example/zoosumx2/MakequizActivity.kt
+++ app/src/main/java/com/example/zoosumx2/MakequizActivity.kt
@@ -3,6 +3,7 @@ package com.example.zoosumx2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 
 class MakequizActivity : AppCompatActivity() {
@@ -11,12 +12,29 @@ class MakequizActivity : AppCompatActivity() {
         setContentView(R.layout.activity_makequiz)
 
         val backButton = findViewById<ImageButton>(R.id.imagebutton_back_general_quiz)
-        backButton.setOnClickListener{
+        backButton.setOnClickListener {
             finish()
         }
 
         val makeQuizButton = findViewById<Button>(R.id.button_makequiz_button_makequiz)
-        makeQuizButton.setOnClickListener{
+
+        val question = findViewById<EditText>(R.id.edittext_question_makequiz)
+        val correctExam = findViewById<EditText>(R.id.edittext_correctans_makequiz)
+        val wrongExam = findViewById<EditText>(R.id.edittext_wrongans_makequiz)
+
+        // 문제 입력, 정답/오답 선지 모두 입력 시에만 퀴즈 출제 버튼 활성화
+        if (question.text.toString().trim().isNotEmpty()) {
+            if (correctExam.text.toString().trim().isNotEmpty()) {
+                if (wrongExam.text.toString().trim().isNotEmpty()) {
+                    makeQuizButton.isEnabled = true
+                }
+            }
+        } else {
+            makeQuizButton.isEnabled = false
+        }
+
+        // 퀴즈 출제 버튼 클릭 이벤트
+        makeQuizButton.setOnClickListener {
             finish()
         }
     }
