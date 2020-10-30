1c958e863d19a8cc315ece52d5e8a97c1f67781d
aerim <aerimforest98@gmail.com>
2020-10-30 13:21:01 +0900

    코드 수정

--- app/src/main/java/com/zoosumzoosum/zoosumx2/dialog/ResidentConfirmSubDialog.kt
+++ app/src/main/java/com/zoosumzoosum/zoosumx2/dialog/ResidentConfirmSubDialog.kt
@@ -21,9 +21,10 @@ import com.google.firebase.storage.ktx.storage
 import java.io.File
 import kotlin.random.Random
 
-class ResidentConfirmSubDialog(context: Context,
-                               val curPhotoPath: String, val timestamp: String?,
-                               val pathString: String
+class ResidentConfirmSubDialog(
+    context: Context,
+    val curPhotoPath: String, val timestamp: String?,
+    val pathString: String
 ) {
     var fbAuth: FirebaseAuth? = null
     var fbFirestore: FirebaseFirestore? = null
@@ -34,7 +35,7 @@ class ResidentConfirmSubDialog(context: Context,
     private val limitInt: Long = 1
     private var nextFlag: Boolean = false
 
-    fun start(context: Context){
+    fun start(context: Context) {
 
         fbAuth = FirebaseAuth.getInstance()
         fbFirestore = FirebaseFirestore.getInstance()
@@ -70,7 +71,8 @@ class ResidentConfirmSubDialog(context: Context,
             )
             fbFirestore?.collection("users")?.document(fbAuth?.uid.toString())
                 ?.collection("mission")?.document(fbAuth?.uid.toString())
-                ?.collection("missionDetail")?.document("recycle")?.set(recyclePhotoInfo, SetOptions.merge())
+                ?.collection("missionDetail")?.document("recycle")
+                ?.set(recyclePhotoInfo, SetOptions.merge())
 
             //재활용 사진 및 정보 <받는 사람의> DB에 업로드
             val randomFlag: ArrayList<String> = ArrayList() //랜덤으로 뽑아올 필드 값 문자열 세팅
@@ -80,82 +82,128 @@ class ResidentConfirmSubDialog(context: Context,
 
             val randomInsertArray: ArrayList<String> = ArrayList() //실제 firestore 접근을 위해 넣을 필드 값
 
-            for(i in 1..3){
+            for (i in 1..3) {
                 val tempInt: Int = Random.nextInt(randomFlag.size)
                 randomInsertArray.add(randomFlag[tempInt])
                 randomFlag.removeAt(tempInt)
-                if(randomFlag.isEmpty())
+                if (randomFlag.isEmpty())
                     break
             }
 
             val randomDescendingFlag: Int = Random.nextInt(8)
             val usersRef = fbFirestore?.collection("users")
 
-            when(randomDescendingFlag){
-                0 -> { usersRef?.orderBy(randomInsertArray[0])?.orderBy(randomInsertArray[1])?.orderBy(randomInsertArray[2])?.limit(limitInt)
-                    ?.get()?.addOnSuccessListener { result ->
-                        for(document in result){
-                            fbFirestore?.collection("users")?.document(document.get("uid").toString())
-                                ?.collection("mission")?.document(document.get("uid").toString())?.update("isReceivedRecycle",fbAuth?.uid.toString())
+            when (randomDescendingFlag) {
+                0 -> {
+                    usersRef?.orderBy(randomInsertArray[0])?.orderBy(randomInsertArray[1])
+                        ?.orderBy(randomInsertArray[2])?.limit(limitInt)
+                        ?.get()?.addOnSuccessListener { result ->
+                            for (document in result) {
+                                fbFirestore?.collection("users")
+                                    ?.document(document.get("uid").toString())
+                                    ?.collection("mission")
+                                    ?.document(document.get("uid").toString())
+                                    ?.update("isReceivedRecycle", fbAuth?.uid.toString())
+                            }
                         }
-                    }}
-                1 -> { usersRef?.orderBy(randomInsertArray[0], Query.Direction.DESCENDING)?.orderBy(randomInsertArray[1])?.orderBy(randomInsertArray[2])?.limit(limitInt)
-                    ?.get()?.addOnSuccessListener { result ->
-                        for(document in result){
-                            fbFirestore?.collection("users")?.document(document.get("uid").toString())
-                                ?.collection("mission")?.document(document.get("uid").toString())?.update("isReceivedRecycle",fbAuth?.uid.toString())
+                }
+                1 -> {
+                    usersRef?.orderBy(randomInsertArray[0], Query.Direction.DESCENDING)
+                        ?.orderBy(randomInsertArray[1])?.orderBy(randomInsertArray[2])
+                        ?.limit(limitInt)
+                        ?.get()?.addOnSuccessListener { result ->
+                            for (document in result) {
+                                fbFirestore?.collection("users")
+                                    ?.document(document.get("uid").toString())
+                                    ?.collection("mission")
+                                    ?.document(document.get("uid").toString())
+                                    ?.update("isReceivedRecycle", fbAuth?.uid.toString())
+                            }
                         }
-                    }}
-                2 -> { usersRef?.orderBy(randomInsertArray[0])?.orderBy(randomInsertArray[1], Query.Direction.DESCENDING)?.orderBy(randomInsertArray[2])?.limit(limitInt)
-                    ?.get()?.addOnSuccessListener { result ->
-                        for(document in result){
-                            fbFirestore?.collection("users")?.document(document.get("uid").toString())
-                                ?.collection("mission")?.document(document.get("uid").toString())?.update("isReceivedRecycle",fbAuth?.uid.toString())
+                }
+                2 -> {
+                    usersRef?.orderBy(randomInsertArray[0])
+                        ?.orderBy(randomInsertArray[1], Query.Direction.DESCENDING)
+                        ?.orderBy(randomInsertArray[2])?.limit(limitInt)
+                        ?.get()?.addOnSuccessListener { result ->
+                            for (document in result) {
+                                fbFirestore?.collection("users")
+                                    ?.document(document.get("uid").toString())
+                                    ?.collection("mission")
+                                    ?.document(document.get("uid").toString())
+                                    ?.update("isReceivedRecycle", fbAuth?.uid.toString())
+                            }
                         }
-                    }}
-                3 -> { usersRef?.orderBy(randomInsertArray[0])?.orderBy(randomInsertArray[1])?.orderBy(randomInsertArray[2], Query.Direction.DESCENDING)?.limit(limitInt)
-                    ?.get()?.addOnSuccessListener { result ->
-                        for(document in result){
-                            fbFirestore?.collection("users")?.document(document.get("uid").toString())
-                                ?.collection("mission")?.document(document.get("uid").toString())?.update("isReceivedRecycle",fbAuth?.uid.toString())
+                }
+                3 -> {
+                    usersRef?.orderBy(randomInsertArray[0])?.orderBy(randomInsertArray[1])
+                        ?.orderBy(randomInsertArray[2], Query.Direction.DESCENDING)?.limit(limitInt)
+                        ?.get()?.addOnSuccessListener { result ->
+                            for (document in result) {
+                                fbFirestore?.collection("users")
+                                    ?.document(document.get("uid").toString())
+                                    ?.collection("mission")
+                                    ?.document(document.get("uid").toString())
+                                    ?.update("isReceivedRecycle", fbAuth?.uid.toString())
+                            }
                         }
-                    }}
-                4 -> { usersRef?.orderBy(randomInsertArray[0], Query.Direction.DESCENDING)
-                    ?.orderBy(randomInsertArray[1], Query.Direction.DESCENDING)
-                    ?.orderBy(randomInsertArray[2])?.limit(limitInt)
-                    ?.get()?.addOnSuccessListener { result ->
-                        for(document in result){
-                            fbFirestore?.collection("users")?.document(document.get("uid").toString())
-                                ?.collection("mission")?.document(document.get("uid").toString())?.update("isReceivedRecycle",fbAuth?.uid.toString())
+                }
+                4 -> {
+                    usersRef?.orderBy(randomInsertArray[0], Query.Direction.DESCENDING)
+                        ?.orderBy(randomInsertArray[1], Query.Direction.DESCENDING)
+                        ?.orderBy(randomInsertArray[2])?.limit(limitInt)
+                        ?.get()?.addOnSuccessListener { result ->
+                            for (document in result) {
+                                fbFirestore?.collection("users")
+                                    ?.document(document.get("uid").toString())
+                                    ?.collection("mission")
+                                    ?.document(document.get("uid").toString())
+                                    ?.update("isReceivedRecycle", fbAuth?.uid.toString())
+                            }
                         }
-                    }}
-                5 -> { usersRef?.orderBy(randomInsertArray[0], Query.Direction.DESCENDING)
-                    ?.orderBy(randomInsertArray[1])
-                    ?.orderBy(randomInsertArray[2], Query.Direction.DESCENDING)?.limit(limitInt)
-                    ?.get()?.addOnSuccessListener { result ->
-                        for(document in result){
-                            fbFirestore?.collection("users")?.document(document.get("uid").toString())
-                                ?.collection("mission")?.document(document.get("uid").toString())?.update("isReceivedRecycle",fbAuth?.uid.toString())
+                }
+                5 -> {
+                    usersRef?.orderBy(randomInsertArray[0], Query.Direction.DESCENDING)
+                        ?.orderBy(randomInsertArray[1])
+                        ?.orderBy(randomInsertArray[2], Query.Direction.DESCENDING)?.limit(limitInt)
+                        ?.get()?.addOnSuccessListener { result ->
+                            for (document in result) {
+                                fbFirestore?.collection("users")
+                                    ?.document(document.get("uid").toString())
+                                    ?.collection("mission")
+                                    ?.document(document.get("uid").toString())
+                                    ?.update("isReceivedRecycle", fbAuth?.uid.toString())
+                            }
                         }
-                    }}
-                6 -> { usersRef?.orderBy(randomInsertArray[0])
-                    ?.orderBy(randomInsertArray[1], Query.Direction.DESCENDING)
-                    ?.orderBy(randomInsertArray[2], Query.Direction.DESCENDING)?.limit(limitInt)
-                    ?.get()?.addOnSuccessListener { result ->
-                        for(document in result){
-                            fbFirestore?.collection("users")?.document(document.get("uid").toString())
-                                ?.collection("mission")?.document(document.get("uid").toString())?.update("isReceivedRecycle",fbAuth?.uid.toString())
+                }
+                6 -> {
+                    usersRef?.orderBy(randomInsertArray[0])
+                        ?.orderBy(randomInsertArray[1], Query.Direction.DESCENDING)
+                        ?.orderBy(randomInsertArray[2], Query.Direction.DESCENDING)?.limit(limitInt)
+                        ?.get()?.addOnSuccessListener { result ->
+                            for (document in result) {
+                                fbFirestore?.collection("users")
+                                    ?.document(document.get("uid").toString())
+                                    ?.collection("mission")
+                                    ?.document(document.get("uid").toString())
+                                    ?.update("isReceivedRecycle", fbAuth?.uid.toString())
+                            }
                         }
-                    }}
-                7 -> { usersRef?.orderBy(randomInsertArray[0], Query.Direction.DESCENDING)
-                    ?.orderBy(randomInsertArray[1], Query.Direction.DESCENDING)
-                    ?.orderBy(randomInsertArray[2], Query.Direction.DESCENDING)?.limit(limitInt)
-                    ?.get()?.addOnSuccessListener { result ->
-                        for(document in result){
-                            fbFirestore?.collection("users")?.document(document.get("uid").toString())
-                                ?.collection("mission")?.document(document.get("uid").toString())?.update("isReceivedRecycle",fbAuth?.uid.toString())
+                }
+                7 -> {
+                    usersRef?.orderBy(randomInsertArray[0], Query.Direction.DESCENDING)
+                        ?.orderBy(randomInsertArray[1], Query.Direction.DESCENDING)
+                        ?.orderBy(randomInsertArray[2], Query.Direction.DESCENDING)?.limit(limitInt)
+                        ?.get()?.addOnSuccessListener { result ->
+                            for (document in result) {
+                                fbFirestore?.collection("users")
+                                    ?.document(document.get("uid").toString())
+                                    ?.collection("mission")
+                                    ?.document(document.get("uid").toString())
+                                    ?.update("isReceivedRecycle", fbAuth?.uid.toString())
+                            }
                         }
-                    }}
+                }
             }
         }
 
@@ -173,7 +221,7 @@ class ResidentConfirmSubDialog(context: Context,
                     btnOk.setBackgroundResource(R.drawable.rounded_rectangle_orange)
                     title.text = "재활용 인증 요청 전송 완료!"
                 }
-            if(nextFlag){
+            if (nextFlag) {
                 dlg.dismiss()
                 val intent = Intent((context as PhotoActivity), MainActivity::class.java)
                 context.startActivity(intent)
