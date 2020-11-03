367441118d671c03d94fc8960da9819ed84ae909
neoAA <fisflower1030@gmail.com>
2020-11-03 21:54:06 +0900

    최초 로그인 시 다시 이메일 입력받지 않기

--- app/src/main/java/com/zoosumzoosum/zoosumx2/LoginActivity.kt
+++ app/src/main/java/com/zoosumzoosum/zoosumx2/LoginActivity.kt
@@ -49,9 +49,13 @@ class LoginActivity : AppCompatActivity() {
         super.onStart()
         val account = GoogleSignIn.getLastSignedInAccount(this)
         // 이미 로그인 한 사용자인 경우
+        if (auth.currentUser != null){
+            moveMainPage(auth.currentUser)
+        }
         if (account != null) {
             moveMainPage(auth.currentUser) // 바로 MainActivity로 이동
         }
+
     }
 
     fun googleLogin() {
